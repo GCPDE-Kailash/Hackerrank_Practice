5ea988d8aabf567e196d41ce49317e5df9bec03f
Babakailash <nathkailash2020@gmail.com>
2021-08-03 02:26:21 +0530

    Added:Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.

--- tapDay2/AVeryBigSumHW.java
+++ tapDay2/AVeryBigSumHW.java
@@ -1,7 +1,10 @@
 package tapDay2;
 
+import java.util.Collections;
+import java.util.List;
+
 public class AVeryBigSumHW {
-    static long aVeryBigSum(long[] ar) {
+    static long aVeryBigSum(List<Long> ar) {
         long sum =0;
         for(long x: ar){
             sum += x;
@@ -10,8 +13,7 @@ public class AVeryBigSumHW {
     }
 
     public static void main(String[] args) {
-        long [] ar = new long[1+3+5+6];
-        System.out.println(aVeryBigSum(ar));
+        long ar = new Long(8);
+        System.out.println(aVeryBigSum(Collections.singletonList(ar)));
     }
-
 }
