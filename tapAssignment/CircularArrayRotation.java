cf801eabed6c2b567a50044f4a9c8eb585acc909
Babakailash <nathkailash2020@gmail.com>
2021-08-05 10:03:52 +0530

    Added:Circular linked list problem, logic parts are correct but driver method not clear

--- tapAssignment/CircularArrayRotation.java
+++ tapAssignment/CircularArrayRotation.java
@@ -1,35 +1,29 @@
 package tapAssignment;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CircularArrayRotation {
-    //public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> q) {
-     /*ArrayList<Integer> b = new ArrayList<>(a.size());
-     k = k%(a.size());
-
-     for (int i =0; i<k; i++){
-         b.get(i) = a.get(a.size()-k+i);
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> q) {
+     k = k%a.size();
+     int l = a.size();
+     for(int i =0; i<q.size();i++){
+         int pos = (q.get(i) -k+l)%l;
+         q.set(i, a.get(pos));
      }
-     for (int i=a.size()-k-1; i>=0; i--){
-         a.get(i+k) = a.get(i);
-     }
-     for (int i =0; i<k; i++){
-         a.get(i)= b.get(i);
-     }
-     for (int i=0; i<q.size(); i++){
-         System.out.println(a.get(q.get(i)));
-     }*/
-
-
-
+     return q;
+    }
 
     public static void main(String[] args) {
-        /*int k =2;
+        // THIS PART IS NOT CLEAR IT'S CIRCULAR LINKED LIST, LOGIC PARTS CORRECT!!
         ArrayList<Integer> a = new ArrayList<>();
         a.add(1);
         a.add(2);
         a.add(3);
-        circularArrayRotation(a,k,q);*/
+        List<Integer> k = Collections.singletonList(2);
+        int q = 3;
+        System.out.println(circularArrayRotation(k,q,a));
+
     }
 }
